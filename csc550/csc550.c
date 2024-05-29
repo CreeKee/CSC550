359df2f0bc91e78a5e53392f99b79fde4964a3e6
Seth Kiefer <70313470+CreeKee@users.noreply.github.com>
2024-05-29 15:12:26 -0700

    Update csc550.c

--- csc550/csc550.c
+++ csc550/csc550.c
@@ -7,13 +7,15 @@
 #include <linux/mutex.h>
 #include <linux/unistd.h>
 #include <linux/syscalls.h>
-
+#include <linux/errno.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
 #include <linux/sched.h>
 #include <asm/uaccess.h>
 
 #define FPGA_SEGMENTS 3
-#define NO_SEGMENTS_AVAILABLE -1
-#define NO_SEGMENTS_RESERVED  -2
+#define BUFFERSIZE 8192
+
 
 #define FPGA_FIRMWARE_PATH "/sys/class/fpga_manager/fpga0/firmware"
 #define FPGA_FLAGS_PATH "/sys/class/fpga_manager/fpga0/flags"
@@ -30,8 +32,12 @@ static int myintArray[2] = { -1, -1 };
 static int arr_argc = 0;
 */
 
+#ifndef FPGA_DIST
+#define FPGA_DIST
+
 static int segments[FPGA_SEGMENTS] = {0};
 static DEFINE_MUTEX(segments_mutex);
+static char buffer[BUFFERSIZE];
 
 /* 
  * module_param(foo, int, 0000)
@@ -67,6 +73,83 @@ module_param_array(myintArray, int, &arr_argc, 0000);
 MODULE_PARM_DESC(myintArray, "An array of integers");
 */
 
+int write_bitstream(const char* bitstream_file_path)
+{
+	int ret = 0;
+	ssize_t size = 0;
+	loff_t pos = 0;
+	
+	struct file* bs = filp_open(bitstream_file_path, O_RDONLY, 0);
+	struct file* pf = filp_open(FPGA_FLAGS_PATH, O_WRONLY | O_TRUNC, 0);
+	struct file* pl = filp_open(FPGA_FIRMWARE_PATH, O_WRONLY | O_TRUNC, 0);
+
+	buffer[0] = 0;
+
+	if(!bs || !pf || !pl) ret = -1;
+
+	else{
+		//do flag write
+		size = kernel_write(pf, buffer, 1, &(pf->f_pos));
+
+		if(size != 1) ret = -1;
+
+		else{
+			do{
+				size = kernel_read(pf, buffer, BUFFERSIZE, &(pos));
+				if(size > 0) ret = kernel_write(pf, buffer, size, &(pl->f_pos));
+				else ret = -1;
+
+			} while(size == BUFFERSIZE && ret > 0);
+		}
+	}
+
+	if(bs) filp_close(bs, NULL);
+	if(pf) filp_close(pf, NULL);
+	if(pl) filp_close(pl, NULL);
+
+	return ret;
+}
+
+inline bool is_holder(int pid)
+{
+
+	bool found = false;
+
+	mutex_lock(&segments_mutex);
+
+	for(int seg = 0; seg < FPGA_SEGMENTS; seg++){
+		if(segments[seg] == pid) found = true;
+	}
+
+	mutex_unlock(&segments_mutex);
+
+	return found;
+}
+
+void exit_fpga(struct task_struct *tsk){
+	
+	int seg;
+	bool done = false;
+
+	mutex_lock(&segments_mutex);
+
+	for(seg = 0; seg < FPGA_SEGMENTS && !done; seg++){
+		if(segments[seg] == tsk->pid){
+
+			done = true;
+			
+			/*clear FPGA?*/
+
+			//mark FPGA segment as available
+			segments[seg] = 0;
+		}
+	}
+
+	mutex_unlock(&segments_mutex);
+
+	return;
+}
+
 int request_fpga_segment_handler(void)
 {
 	int seg;
@@ -86,7 +169,7 @@ int request_fpga_segment_handler(void)
 
 	mutex_unlock(&segments_mutex);
 
-	if(!done) seg = NO_SEGMENTS_AVAILABLE;
+	if(!done) seg = -ENOSEGMENTSAVAILABLE;
 
 	if(seg >= 0){
 		/*do any additional setup*/
@@ -95,10 +178,20 @@ int request_fpga_segment_handler(void)
 	return seg;
 }
 
-int program_fpga_segment_handler(char* bitstream_file_name)
+int program_fpga_segment_handler(const char* bitstream_file_path)
 {
-	//echo 0 > 
-	return 0;
+
+	int ret = -ENOEGMENTSRESERVED;
+/*
+	mutex_lock(&segments_mutex);
+
+	if(is_holder(current->pid)){
+		ret = write_bitstream(bitstream_file_path);
+	}
+
+	mutex_unlock(&segments_mutex);
+*/
+	return ret;
 }
 
 int release_fpga_segment_handler(void)
@@ -125,8 +218,7 @@ int release_fpga_segment_handler(void)
 
 	mutex_unlock(&segments_mutex);
 
-	if(!done) seg = NO_SEGMENTS_RESERVED;
-	else seg = 1;
+	if(!done) seg = -ENOEGMENTSRESERVED;
 
 	if(seg >= 0){
 		/*do any additional setup*/
@@ -140,14 +232,17 @@ SYSCALL_DEFINE0(request_fpga_segment)
 	return request_fpga_segment_handler();
 }
 
-SYSCALL_DEFINE1(program_fpga_segment, char *, filename)
+
+SYSCALL_DEFINE1(program_fpga_segment, const char *, filename)
 {
+	
 	return program_fpga_segment_handler(filename);
 }
 
-SYSCALL_DEFINE0(program_fpga_segment)
+
+SYSCALL_DEFINE0(release_fpga_segment)
 {
-	return program_fpga_segment_handler();
+	return release_fpga_segment_handler();
 }
 
 /*
@@ -167,3 +262,5 @@ module_init(request_fpga_segment_handler_init);
 module_exit(request_fpga_segment_handler_exit);
 
 */
+
+#endif
